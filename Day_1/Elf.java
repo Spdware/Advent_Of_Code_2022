ff6f80b31060789219be03d877a76af169967df7
Spdware <70917350+Spdware@users.noreply.github.com>
2022-12-02 00:15:06 +0100

    Add files via upload

---
+++ Day_1/Elf.java
@@ -0,0 +1,12 @@
+public class Elf{
+    int calorie;
+    public Elf(int calorie){
+        this.calorie = calorie;
+    }
+
+    private void setCalorie(int calorie){ this.calorie = calorie;   }
+
+    public int getCalorie(){ return this.calorie;}
+
+    
+}
