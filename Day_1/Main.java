67f79d1d1e36a2f6d90202e38bbe2b8a6a1307c5
Spdware <70917350+Spdware@users.noreply.github.com>
2022-12-02 10:30:53 +0100

    Add files via upload

---
+++ Day_1/Main.java
@@ -0,0 +1,24 @@
+package Day_1;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main{
+
+  public static void main(String[] args){
+    Integer maxCalories = 0, tmp = 0;
+    Scanner inputStream = null;
+    try{
+      inputStream = new Scanner(new File("Test.txt"));
+    }catch(FileNotFoundException e){
+      System.out.println("File non trovato");
+      System.exit(0);
+    }
+    while(inputStream.hasNextLine()){
+      String row = inputStream.nextLine();
+      if(row.isEmpty()){if(tmp > maxCalories) maxCalories = tmp;tmp = 0;}
+      else{tmp += Integer.parseInt(row);}}
+    System.out.println(maxCalories);
+  }
+}
