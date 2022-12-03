8091f6a284996895d05ffbc069bea4de27047ad8
Spdware <70917350+Spdware@users.noreply.github.com>
2022-12-03 11:31:37 +0100

    Add files via upload

---
+++ Day_3/Main.java
@@ -0,0 +1,27 @@
+package Day_3;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner inputStream = null;
+    Integer priority = 0;
+    try{
+      inputStream = new Scanner(new File("Test.txt"));
+    }catch(FileNotFoundException e){
+      System.out.println("File non trovato");
+      System.exit(0);
+    }
+    while(inputStream.hasNextLine()) {
+      Rucksack rucksack =new Rucksack(inputStream.nextLine());
+      priority += rucksack.itemNeedingReallocation();
+    }
+    System.out.println(priority);
+    inputStream.close();
+
+  }
+
+}
+
---
+++ Day_3/Rucksack.java
@@ -0,0 +1,23 @@
+package Day_3;
+
+public class Rucksack {
+  private final String firstContainer;
+  private final String secondContainer;
+
+  public Rucksack(String container){
+    this.firstContainer =  container.substring(0, (container.length() / 2));
+    this.secondContainer = container.substring(container.length() / 2);
+
+  }
+
+  public int itemNeedingReallocation(){
+    for(char c : firstContainer.toCharArray()){
+      if(secondContainer.indexOf(c) != -1)
+        return (c >= 65 && c <= 90)? (c - 64 + 26) : c - 96;
+    }
+    System.out.println("Errore "+ firstContainer + secondContainer);
+    return 0;
+  }
+}
+
+
